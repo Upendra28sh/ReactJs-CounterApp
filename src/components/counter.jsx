504555d2b02d6d13f2132a4ad13edf95ecf53858
upendra <upendra861@gmail.com>
2018-09-24 23:46:18 +0530

    Update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,7 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
+    console.log();
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -15,7 +16,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
